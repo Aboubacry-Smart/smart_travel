e7c82a6b0fab3dc23fd7164b8c9eab65f3b05cb2
Tall <aboubacry.tall@smartdev.ai>
2025-09-22 02:06:26 +0000

    Initial Ticket Search

--- travel_management/controllers/travel_booking_ticket.py
+++ travel_management/controllers/travel_booking_ticket.py
@@ -2,14 +2,18 @@ from odoo import http
 from odoo.http import request
 
 class TravelBookingTicketController(http.Controller):
+    @http.route(['/my/travel/ticket'], type='http', auth='public', website=True)
+    def ticket(self, **kwargs):
+        return request.render('travel_management.travel_booking_ticket')
+
     @http.route(['/my/travel/ticket/<string:code>'], type='http', auth='public', website=True)
     def ticket_status(self, code, **kwargs):
         Order = request.env['travel.order'].sudo()
         order = Order.search([('code', '=', code)], order='id desc', limit=1)
 
-        context = {
+        values = {
             'found': bool(order),
             'order': order,
             'status': order.state if order else None,
         }
-        return request.render('travel_management.travel_booking_ticket', context)
+        return request.render('travel_management.travel_booking_ticket', values)
